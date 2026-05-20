c8d45e762db17da13bb387fdd4acc0fdd7d13a0a
Stephy1913 <Stefy.1.11@icloud.com>
2026-05-20 12:08:08 -0600

    Create 00_download_kaggle.py

---
+++ cloud_pipeline/scripts/00_download_kaggle.py
@@ -0,0 +1,33 @@
+import os
+import zipfile
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+DATASET = "mehmettahiraslan/customer-shopping-dataset"
+DATA_DIR = "data"
+ZIP_FILE = os.path.join(DATA_DIR, "customer-shopping-dataset.zip")
+
+
+def main():
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+    print("Autenticando con Kaggle...")
+    api = KaggleApi()
+    api.authenticate()
+
+    print("Descargando dataset desde Kaggle...")
+    api.dataset_download_files(
+        DATASET,
+        path=DATA_DIR,
+        unzip=False
+    )
+
+    print("Descomprimiendo archivo...")
+    with zipfile.ZipFile(ZIP_FILE, "r") as zip_ref:
+        zip_ref.extractall(DATA_DIR)
+
+    print("Dataset descargado correctamente en la carpeta data/")
+
+
+if __name__ == "__main__":
+    main()
